8f181345bc5e4e23b770039ad9289a15cf31c7a7
HaiChi <chenxidev1129@gmail.com>
2023-04-27 19:46:21 +0800

    hidden all origin code

--- m/main/index.php
+++ m/main/index.php
@@ -111,7 +111,7 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 
 <!-- 안내메뉴 -->
 <div class="sub-menu">
-	<a href="#" class="menu-item">
+	<a href="../cscenter/guide.php" class="menu-item">
 		<img src="../images/menu_item1.png" alt="menu_item1" />
 		<p class="item-title">이용안내</p>
 	</a>
@@ -235,7 +235,7 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 	</div>
 </div>
 
-<!-- Rent Limit -->
+<!-- 렌트 한정 기간 타임세일 -->
 <div class="rentlimit">
 	<div class="sub-section-top-bar">
 		<div class="left-section">
@@ -274,7 +274,7 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 	</div>
 </div>
 
-<!-- Rent New -->
+<!-- 렌트 신규 입고 -->
 <div class="rentnew">
 	<div class="sub-section-top-bar">
 		<div class="left-section">
@@ -313,7 +313,7 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 	</div>
 </div>
 
-<!-- Subscription New -->
+<!-- 구독 신규 입고 -->
 <div class="subscriptionnew">
 	<div class="sub-section-top-bar">
 		<div class="left-section">
@@ -379,11 +379,13 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 	<button class="review-button">더 많은 리뷰 보러가기</button>
 </div>
 
-<div class="con_width">
-	<div class="main_tit2"><span>NEW</span></div>
-	<ul class="new_list">
-		<?
-		$SQL_QUERY = "SELECT 
+<!-- hidden by kkk -->
+<div style="display: none;">
+	<div class="con_width">
+		<div class="main_tit2"><span>NEW</span></div>
+		<ul class="new_list">
+			<?
+			$SQL_QUERY = "SELECT 
 							A.*,
 							(SELECT B.STR_BCODE FROM " . $Tname . "comm_goods_master_category B WHERE A.STR_GOODCODE=B.STR_GOODCODE LIMIT 1) AS STR_BCODE,
 							(SELECT IFNULL(COUNT(Z.STR_USERID),0) AS CNT FROM " . $Tname . "comm_member_like Z WHERE Z.STR_GOODCODE=A.STR_GOODCODE) AS LIKECNT, 
@@ -404,176 +406,172 @@ $arr_Get_Data_Cnt5 = mysql_num_rows($arr_Get_Data5);
 						ORDER BY 
 							A.INT_SORT DESC";
 
-		$arr_Data = mysql_query($SQL_QUERY);
-		$arr_Data_Cnt = mysql_num_rows($arr_Data);
-		?>
-		<? $sBuy = fnc_buy_info(); ?>
+			$arr_Data = mysql_query($SQL_QUERY);
+			$arr_Data_Cnt = mysql_num_rows($arr_Data);
+			?>
+			<? $sBuy = fnc_buy_info(); ?>
+			<?
+			for ($int_J = 0; $int_J < $arr_Data_Cnt; $int_J++) {
+			?>
+				<? $sRent = fnc_cart_info(mysql_result($arr_Data, $int_J, str_goodcode)); ?>
+				<li>
+					<? if ($sRent == 0) { ?>
+						<span class="rented">RENTED</span>
+					<? } ?>
+					<p class="zzim_icn"><img src="../images/icn_zzim_on.png" alt="" /> <?= mysql_result($arr_Data, $int_J, likecnt) ?></p>
+					<a href="/m/category/detail.php?Txt_bcode=<?= mysql_result($arr_Data, $int_J, str_bcode) ?>&str_no=<?= mysql_result($arr_Data, $int_J, str_goodcode) ?>">
+						<p><? if (mysql_result($arr_Data, $int_J, str_image1) != "") { ?><img src="/admincenter/files/good/<?= mysql_result($arr_Data, $int_J, str_image1) ?>" border="0"><? } ?></p>
+						<dl>
+							<dt><?= mysql_result($arr_Data, $int_J, str_code) ?></dt>
+							<dd><?= mysql_result($arr_Data, $int_J, str_egoodname) ?></dd>
+						</dl>
+					</a>
+				</li>
+			<?
+			}
+			?>
+
+		</ul>
+		<p><a href="/m/category/list.php" class="btn btn_readmore">MORE BAGS <i class="icn"></i></a></p>
+		<div class="banner_line2 mt50"></div>
+		<div class="main_tit">
+			<span>BRAND</span>
+		</div>
+		<div class="m_brand">
+			<ul>
+				<?
+				$SQL_QUERY = "select a.* from " . $Tname . "comm_com_code a where a.str_service='Y' and a.int_gubun='2' order by a.int_number asc ";
+
+				$arr_Bann_Data = mysql_query($SQL_QUERY);
+				$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
+				?>
+				<?
+				for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
+				?>
+					<li><a href="/m/search/search.php?Txt_brand[]=<?= mysql_result($arr_Bann_Data, $int_J, int_number) ?>"><span><? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?><img src="/admincenter/files/com/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" /><? } ?></span></a></li>
+				<?
+				}
+				?>
+			</ul>
+		</div>
+
+	</div>
+	<div class="banner_line2 mt50">
 		<?
-		for ($int_J = 0; $int_J < $arr_Data_Cnt; $int_J++) {
+		$SQL_QUERY = "select a.* from " . $Tname . "comm_banner a where a.str_service='Y' and a.int_gubun='4' order by a.int_number asc ";
+
+		$arr_Bann_Data = mysql_query($SQL_QUERY);
+		$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
 		?>
-			<? $sRent = fnc_cart_info(mysql_result($arr_Data, $int_J, str_goodcode)); ?>
-			<li>
-				<? if ($sRent == 0) { ?>
-					<span class="rented">RENTED</span>
-				<? } ?>
-				<p class="zzim_icn"><img src="../images/icn_zzim_on.png" alt="" /> <?= mysql_result($arr_Data, $int_J, likecnt) ?></p>
-				<a href="/m/category/detail.php?Txt_bcode=<?= mysql_result($arr_Data, $int_J, str_bcode) ?>&str_no=<?= mysql_result($arr_Data, $int_J, str_goodcode) ?>">
-					<p><? if (mysql_result($arr_Data, $int_J, str_image1) != "") { ?><img src="/admincenter/files/good/<?= mysql_result($arr_Data, $int_J, str_image1) ?>" border="0"><? } ?></p>
-					<dl>
-						<dt><?= mysql_result($arr_Data, $int_J, str_code) ?></dt>
-						<dd><?= mysql_result($arr_Data, $int_J, str_egoodname) ?></dd>
-					</dl>
-				</a>
-			</li>
+		<?
+		for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
+		?>
+			<? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?>
+				<p>
+					<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
+						<a href="<?= mysql_result($arr_Bann_Data, $int_J, str_url1) ?>" <? if (mysql_result($arr_Bann_Data, $int_J, str_target1) == "2") { ?> target="_blank" <? } ?>>
+						<? } ?>
+						<img src="/admincenter/files/bann/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" />
+						<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
+						</a>
+					<? } ?>
+				</p>
+			<? } ?>
+		<?
+		}
+		?>
+	</div>
+	<div class="con_width" style="padding-bottom: 30px;">
+		<?
+		$SQL_QUERY = "select a.* from " . $Tname . "comm_banner a where a.str_service='Y' and a.int_gubun='5' order by a.int_number asc ";
+
+		$arr_Bann_Data = mysql_query($SQL_QUERY);
+		$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
+		?>
+		<?
+		for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
+		?>
+			<? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?>
+				<p class="m_service">
+					<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
+						<a href="<?= mysql_result($arr_Bann_Data, $int_J, str_url1) ?>" <? if (mysql_result($arr_Bann_Data, $int_J, str_target1) == "2") { ?> target="_blank" <? } ?>>
+						<? } ?>
+						<img src="/admincenter/files/bann/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" />
+						<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
+						</a>
+					<? } ?>
+				</p>
+			<? } ?>
 		<?
 		}
 		?>
 
-	</ul>
-	<p><a href="/m/category/list.php" class="btn btn_readmore">MORE BAGS <i class="icn"></i></a></p>
-	<div class="banner_line2 mt50"></div>
-	<div class="main_tit">
-		<span>BRAND</span>
-	</div>
-	<div class="m_brand">
-		<ul>
-			<?
-			$SQL_QUERY = "select a.* from " . $Tname . "comm_com_code a where a.str_service='Y' and a.int_gubun='2' order by a.int_number asc ";
 
-			$arr_Bann_Data = mysql_query($SQL_QUERY);
-			$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
-			?>
-			<?
-			for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
-			?>
-				<li><a href="/m/search/search.php?Txt_brand[]=<?= mysql_result($arr_Bann_Data, $int_J, int_number) ?>"><span><? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?><img src="/admincenter/files/com/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" /><? } ?></span></a></li>
-			<?
-			}
-			?>
-		</ul>
-	</div>
-
-</div>
-<div class="banner_line2 mt50">
-	<?
-	$SQL_QUERY = "select a.* from " . $Tname . "comm_banner a where a.str_service='Y' and a.int_gubun='4' order by a.int_number asc ";
-
-	$arr_Bann_Data = mysql_query($SQL_QUERY);
-	$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
-	?>
-	<?
-	for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
-	?>
-		<? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?>
-			<p>
-				<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
-					<a href="<?= mysql_result($arr_Bann_Data, $int_J, str_url1) ?>" <? if (mysql_result($arr_Bann_Data, $int_J, str_target1) == "2") { ?> target="_blank" <? } ?>>
-					<? } ?>
-					<img src="/admincenter/files/bann/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" />
-					<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
-					</a>
-				<? } ?>
-			</p>
-		<? } ?>
-	<?
-	}
-	?>
-</div>
-<div class="con_width" style="padding-bottom: 30px;">
-	<?
-	$SQL_QUERY = "select a.* from " . $Tname . "comm_banner a where a.str_service='Y' and a.int_gubun='5' order by a.int_number asc ";
-
-	$arr_Bann_Data = mysql_query($SQL_QUERY);
-	$arr_Bann_Data_Cnt = mysql_num_rows($arr_Bann_Data);
-	?>
-	<?
-	for ($int_J = 0; $int_J < $arr_Bann_Data_Cnt; $int_J++) {
-	?>
-		<? if (mysql_result($arr_Bann_Data, $int_J, str_image1) != "") { ?>
-			<p class="m_service">
-				<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
-					<a href="<?= mysql_result($arr_Bann_Data, $int_J, str_url1) ?>" <? if (mysql_result($arr_Bann_Data, $int_J, str_target1) == "2") { ?> target="_blank" <? } ?>>
-					<? } ?>
-					<img src="/admincenter/files/bann/<?= mysql_result($arr_Bann_Data, $int_J, str_image1) ?>" alt="" />
-					<? if (mysql_result($arr_Bann_Data, $int_J, str_url1) != "") { ?>
-					</a>
-				<? } ?>
-			</p>
-		<? } ?>
-	<?
-	}
-	?>
-
-
-	<div class="modal__wrapper is-hidden js-example-modal-1">
-		<div class="modal__double js-modal__double">
-			<div class="modal__content">
-				<a href="#" class="modal__close js-modal__close"></a>
-				<div class="contents_bx">
-					<p style="height:315px;overflow-y:scroll;border:1px solid #ccc;"><img src="../images/membership_guide_mobile02.gif" alt="멤버십 이용 가이드" /></p>
-					<p class="center mt10"><a href="/m/mypage/membership.php" class="btn btn_bk btn_s">멤버십 등록하러 가기</a></p>
+		<div class="modal__wrapper is-hidden js-example-modal-1">
+			<div class="modal__double js-modal__double">
+				<div class="modal__content">
+					<a href="#" class="modal__close js-modal__close"></a>
+					<div class="contents_bx">
+						<p style="height:315px;overflow-y:scroll;border:1px solid #ccc;"><img src="../images/membership_guide_mobile02.gif" alt="멤버십 이용 가이드" /></p>
+						<p class="center mt10"><a href="/m/mypage/membership.php" class="btn btn_bk btn_s">멤버십 등록하러 가기</a></p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<link rel="stylesheet" href="/m/css/simplePop.css">
+		<link rel="stylesheet" href="/m/css/simplePop.css">
 
-	<script src="/m/js/simplePopup.js"></script>
-	<script type="text/javascript">
-		$(function() {
+		<script src="/m/js/simplePopup.js"></script>
+		<script type="text/javascript">
+			$(function() {
 
-			$('.js-open-modal').each(function() {
+				$('.js-open-modal').each(function() {
 
-				var modalClass = $(this).data('what');
-				var $modal = $('.' + modalClass);
-				var $this = $(this);
+					var modalClass = $(this).data('what');
+					var $modal = $('.' + modalClass);
+					var $this = $(this);
 
 
-				var code = $modal.html();
-				var textarea = $this.parents('.example__item').append('<div class="example__code"><textarea></textarea></div>').find('textarea');
-				textarea.val(code);
+					var code = $modal.html();
+					var textarea = $this.parents('.example__item').append('<div class="example__code"><textarea></textarea></div>').find('textarea');
+					textarea.val(code);
 
-				$this.on('click', function() {
-					$modal.simplePop();
+					$this.on('click', function() {
+						$modal.simplePop();
+					});
+
 				});
 
 			});
-
-		});
-	</script>
-</div>
-<div class="main_banner">
-	<a href="https://www.instagram.com/ablanc_lookbook/?hl=ko"><img src="../images/instagram.jpg" /></a>
-	<a href="/m/cscenter/lookbook1.php"><img src="../images/lookbook.jpg" style="padding-top:10px;" /><a>
-			<a href="/m/cscenter/guideyong.php"><img src="../images/introduce.jpg" style="padding-top:10px; padding-bottom:10px;" /></a>
-</div>
-
-<div>
-	<a href="/m/cscenter/guideyong.php"><img src="../images/helper1.jpg" /></a>
-</div>
-
-<div>
-	<div style="width:50%; float:left;">
-		<a href="/m/cscenter/deliveryyong.php"><img src="../images/helper2.jpg" /></a>
+		</script>
 	</div>
-	<div style="width:50%; float:left;">
-		<a href="/m/cscenter/updateyong.php"><img src="../images/helper3.jpg" /></a>
+	<div class="main_banner">
+		<a href="https://www.instagram.com/ablanc_lookbook/?hl=ko"><img src="../images/instagram.jpg" /></a>
+		<a href="/m/cscenter/lookbook1.php"><img src="../images/lookbook.jpg" style="padding-top:10px;" /></a>
+		<a href="/m/cscenter/guideyong.php"><img src="../images/introduce.jpg" style="padding-top:10px; padding-bottom:10px;" /></a>
 	</div>
-</div>
-<div>
-	<div style="width:50%; float:left;">
-		<a href="/m/cscenter/faq.php"><img src="../images/helper4.jpg" /></a>
+	<div>
+		<a href="/m/cscenter/guideyong.php"><img src="../images/helper1.jpg" /></a>
 	</div>
-	<div style="width:50%; float:left;">
-		<a href="/m/cscenter/csyong.php"><img src="../images/helper5.jpg" /></a>
+	<div>
+		<div style="width:50%; float:left;">
+			<a href="/m/cscenter/deliveryyong.php"><img src="../images/helper2.jpg" /></a>
+		</div>
+		<div style="width:50%; float:left;">
+			<a href="/m/cscenter/updateyong.php"><img src="../images/helper3.jpg" /></a>
+		</div>
 	</div>
-</div>
-
-
-<div>
-	<a href="/m/cscenter/guideyong.php"><img src="../images/bottom_banner.jpg" alt="멤버십 이용 가이드" /></a>
-
+	<div>
+		<div style="width:50%; float:left;">
+			<a href="/m/cscenter/faq.php"><img src="../images/helper4.jpg" /></a>
+		</div>
+		<div style="width:50%; float:left;">
+			<a href="/m/cscenter/csyong.php"><img src="../images/helper5.jpg" /></a>
+		</div>
+	</div>
+	<div>
+		<a href="/m/cscenter/guideyong.php"><img src="../images/bottom_banner.jpg" alt="멤버십 이용 가이드" /></a>
+	</div>
 </div>
 
 
