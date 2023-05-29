2879197d15aa6c8f8aa59872b23202748e20e4b6
HaiChi <chenxidev1129@gmail.com>
2023-05-29 16:13:26 +0800

    update

--- m/mine/user/index.php
+++ m/mine/user/index.php
@@ -267,12 +267,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/m/inc/header_detail.php";
 			</svg>
 		</a>
 		<!-- 적립금 현황 -->
-		<div class="flex flex-row justify-between items-center px-[5px] py-2.5 border-b-[0.5px] border-[#E0E0E0]">
+		<a href="/m/mine/reserve/index.php" class="flex flex-row justify-between items-center px-[5px] py-2.5 border-b-[0.5px] border-[#E0E0E0]">
 			<p class="font-bold text-xs leading-[140%] text-[#666666]">적립금 현황</p>
 			<svg width="6" height="9" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path d="M1.18894 8.8219L5.84793 4.83245C5.90323 4.78496 5.9423 4.73351 5.96516 4.6781C5.98839 4.62269 6 4.56332 6 4.5C6 4.43668 5.98839 4.37731 5.96516 4.3219C5.9423 4.26649 5.90323 4.21504 5.84793 4.16755L1.18894 0.166227C1.05991 0.0554089 0.898617 0 0.705069 0C0.51152 0 0.345623 0.0593668 0.207374 0.1781C0.0691247 0.296834 0 0.435356 0 0.593668C0 0.751979 0.0691247 0.890501 0.207374 1.00923L4.27189 4.5L0.207374 7.99077C0.078341 8.10158 0.0138249 8.23805 0.0138249 8.40016C0.0138249 8.56259 0.0829492 8.70317 0.221198 8.8219C0.359447 8.94063 0.520737 9 0.705069 9C0.8894 9 1.05069 8.94063 1.18894 8.8219Z" fill="#333333" />
 			</svg>
-		</div>
+		</a>
 		<!-- 쿠폰 현황 -->
 		<a href="/m/mine/coupon/index.php" class="flex flex-row justify-between items-center px-[5px] py-2.5 border-b-[0.5px] border-[#E0E0E0]">
 			<p class="font-bold text-xs leading-[140%] text-[#666666]">쿠폰 현황</p>
