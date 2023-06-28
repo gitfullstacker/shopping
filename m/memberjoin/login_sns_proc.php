ea29c00b4eec93d033b40848329a73e880d3a205
HaiChi <chenxidev1129@gmail.com>
2023-06-28 12:09:59 +0800

    update

--- m/memberjoin/login_sns_proc.php
+++ m/memberjoin/login_sns_proc.php
@@ -7,7 +7,8 @@ $user_info = $_GET['user_info'];
 
 // decode user information from JSON format
 $user = json_decode($user_info, true);
-
+var_dump($user);
+exit;
 if ($user['resultcode'] != '00') {
 ?>
 	<script>
