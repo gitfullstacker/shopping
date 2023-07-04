645e16e975e9bfb83e81f96268dffaa5aab5fa79
Hai Chi <chenxidev1129@gmail.com>
2023-07-04 10:57:46 +0800

    update

--- m/mine/order/order_proc.php
+++ m/mine/order/order_proc.php
@@ -7,7 +7,7 @@ $d_date = Fnc_Om_Conv_Default($_REQUEST['d_date'], '');
 
 switch ($RetrieveFlag) {
     case "CANCELORDER":
-        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=11, DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "' WHERE INT_NUMBER=" . $int_cart;
+        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=11, DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "' WHERE INT_NUMBER='" . $int_cart . "'";
         mysql_query($Sql_Query);
         echo 'successful';
 
@@ -15,14 +15,14 @@ switch ($RetrieveFlag) {
         break;
 
     case "RETURNORDER":
-        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=5, STR_RDATE='" . $d_date . "', DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "' WHERE INT_NUMBER=" . $int_cart;
+        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=5, STR_RDATE='" . $d_date . "', DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "' WHERE INT_NUMBER='" . $int_cart . "'";
         mysql_query($Sql_Query);
         echo 'successful';
 
         exit;
         break;
     case "RECEIVEDORDER":
-        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=6 WHERE INT_NUMBER=" . $int_cart . ", DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "'";
+        $Sql_Query = "UPDATE `" . $Tname . "comm_goods_cart` SET INT_STATE=6 WHERE INT_NUMBER='" . $int_cart . "', DTM_EDIT_DATE='" . date("Y-m-d H:i:s") . "'";
         mysql_query($Sql_Query);
         echo 'successful';
 
