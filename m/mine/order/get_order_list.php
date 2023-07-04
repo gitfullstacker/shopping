ca2b640a4434037c7503119264a6532e81a31927
Hai Chi <chenxidev1129@gmail.com>
2023-07-04 12:54:23 +0800

    update

--- m/mine/order/get_order_list.php
+++ m/mine/order/get_order_list.php
@@ -119,10 +119,7 @@ if ($end_page > 0) {
                         <button class="w-full h-10 flex justify-center items-center bg-white border border-solid border-[#DDDDDD] rounded-[3px]" onclick="openDeliveryDialog(\'' . $row['STR_DELICODE'] . '\');">
                             <p class="font-bold text-xs leading-[14px] text-center text-[#666666]">배송 조회</p>
                         </button>
-                        <button class="w-full h-10 flex justify-center items-center bg-white border border-solid border-[#DDDDDD] rounded-[3px]" onclick="returnOrder(\'' . $row['INT_NUMBER'] . '\')">
-                            <p class="font-bold text-xs leading-[14px] text-center text-[#666666]">반납 신청</p>
-                        </button>
-                        <a href="/m/mine/question/create.php?int_cart=' . $row['INT_NUMBER'] . '" class="w-full h-10 flex justify-center items-center bg-white border border-solid border-[#DDDDDD] rounded-[3px] col-span-2">
+                        <a href="/m/mine/question/create.php?int_cart=' . $row['INT_NUMBER'] . '" class="w-full h-10 flex justify-center items-center bg-white border border-solid border-[#DDDDDD] rounded-[3px]">
                             <p class="font-bold text-xs leading-[14px] text-center text-[#666666]">1:1 문의</p>
                         </a>
                     ';
