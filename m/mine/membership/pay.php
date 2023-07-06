90184e239a3e8f54198205d7fb3e2878ed168eff
Hai Chi <chenxidev1129@gmail.com>
2023-07-06 20:28:47 +0800

    update

--- m/mine/membership/pay.php
+++ m/mine/membership/pay.php
@@ -359,28 +359,22 @@ $payment_Data = mysql_fetch_assoc($arr_Rlt_Data);
     </div>
 
     <!-- 약관동의 -->
-    <?php
-    if ($int_type != 1) {
-    ?>
-        <div class="mt-4 flex flex-col gap-2.5 px-[14px]">
-            <div class="flex justify-between items-center">
-                <div class="flex gap-[5px] items-center">
-                    <input type="checkbox" name="agree_terms" id="agree_terms" class="w-[14px] h-[14px] accent-black">
-                    <label for="agree_terms" class="font-bold text-xs leading-[14px] text-[#666666]">보증금 약관 동의하기</label>
-                </div>
-                <a href="/m/help/deposit_agree.php" class="font-medium text-[10px] leading-3 text-right underline text-[#666666]">약관보기</a>
+    <div class="mt-4 flex flex-col gap-2.5 px-[14px]">
+        <div class="flex justify-between items-center">
+            <div class="flex gap-[5px] items-center">
+                <input type="checkbox" name="agree_terms" id="agree_terms" class="w-[14px] h-[14px] accent-black">
+                <label for="agree_terms" class="font-bold text-xs leading-[14px] text-[#666666]">보증금 약관 동의하기</label>
             </div>
-            <div class="flex justify-between items-center">
-                <div class="flex gap-[5px] items-center">
-                    <input type="checkbox" name="agree_payment" id="agree_payment" class="w-[14px] h-[14px] accent-black">
-                    <label for="agree_payment" class="font-bold text-xs leading-[14px] text-[#666666]">약관 및 개인정보 제 3자 제공사항 결제 동의하기</label>
-                </div>
-                <a href="/m/help/privacy_agree.php" class="font-medium text-[10px] leading-3 text-right underline text-[#666666]">약관보기</a>
-            </div>
+            <a href="/m/help/deposit_agree.php" class="font-medium text-[10px] leading-3 text-right underline text-[#666666]">약관보기</a>
         </div>
-    <?php
-    }
-    ?>
+        <div class="flex justify-between items-center">
+            <div class="flex gap-[5px] items-center">
+                <input type="checkbox" name="agree_payment" id="agree_payment" class="w-[14px] h-[14px] accent-black">
+                <label for="agree_payment" class="font-bold text-xs leading-[14px] text-[#666666]">약관 및 개인정보 제 3자 제공사항 결제 동의하기</label>
+            </div>
+            <a href="/m/help/privacy_agree.php" class="font-medium text-[10px] leading-3 text-right underline text-[#666666]">약관보기</a>
+        </div>
+    </div>
 
     <!-- 하단 메뉴 -->
     <div class="fixed bottom-0 w-full flex h-[66px] max-w-[410px]">
