a07322a8573c49fa53c09a7cd2dbf25e93e6fe52
Hai Chi <chenxidev1129@gmail.com>
2023-07-07 02:32:34 +0800

    update

--- m/mine/membership/cancel.php
+++ m/mine/membership/cancel.php
@@ -9,27 +9,19 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/m/inc/header_detail.php";
 <?php
 $int_type = $_GET['int_type'];
 
-$SQL_QUERY =	"SELECT 
-                    B.*
-                FROM `"
-                    .$Tname."comm_member_pay` AS A
-                INNER JOIN
-                    `".$Tname."comm_member_pay_info` AS B
-                ON
-                    A.INT_NUMBER=B.INT_NUMBER
-                    AND 
-                    A.STR_PASS='0' 
-                    AND
-                    date_format(B.STR_SDATE, '%Y-%m-%d') <= '".date("Y-m-d")."'
-                    AND
-                    date_format(B.STR_EDATE, '%Y-%m-%d') >= '".date("Y-m-d")."' 
-                    AND
-                    A.STR_USERID='$arr_Auth[0]'
-                    AND
-                    B.INT_TYPE=" . $int_type;
+//멤버십정보얻기
+$SQL_QUERY =    'SELECT 
+                    A.*
+                FROM 
+                    `' . $Tname . 'comm_membership` A
+                WHERE 
+                    A.STR_USERID = "' . $arr_Auth[0] . '"
+					AND NOW() BETWEEN A.DTM_SDATE AND A.DTM_EDATE
+                    AND A.STR_PASS = "0"
+                    AND A.INT_TYPE = ' . $int_type;
 
-$arr_Rlt_Data=mysql_query($SQL_QUERY);
-$arr_Data=mysql_fetch_assoc($arr_Rlt_Data);
+$arr_Rlt_Data = mysql_query($SQL_QUERY);
+$arr_Data = mysql_fetch_assoc($arr_Rlt_Data);
 ?>
 
 <div class="mt-[30px] flex flex-col items-center w-full px-[14px]">
@@ -40,7 +32,7 @@ $arr_Data=mysql_fetch_assoc($arr_Rlt_Data);
         </p>
         <div class="mt-[13px] flex py-3 justify-center border-t-[0.5px] border-b-[0.5px] border-[#E0E0E0] w-full">
             <p class="font-semibold text-[15px] leading-[18px] text-[#6A696C]">
-                멤버십 종료 예정일: <span class="text-black"><?= date('Y. m. d', strtotime($arr_Data['STR_EDATE'])) ?></span>
+                멤버십 종료 예정일: <span class="text-black"><?= date('Y. m. d', strtotime($arr_Data['DTM_EDATE'])) ?></span>
             </p>
         </div>
         <div class="mt-[15px] flex flex-col gap-[7px] w-full bg-[#F5F5F5] px-[9px] py-[15px]">
