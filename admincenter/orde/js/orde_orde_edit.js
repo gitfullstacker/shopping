745ce125d1ba7b2e3279c31e31f1a8577a57918b
Hai Chi <chenxidev1129@gmail.com>
2023-07-08 14:03:11 +0800

    update

--- admincenter/orde/js/orde_orde_edit.js
+++ admincenter/orde/js/orde_orde_edit.js
@@ -1,30 +1,32 @@
-	function Save_Click(int_type) {
-		if (int_type != 3 && ValidChk()==false) return;
-
-		if (document.frm.RetrieveFlag.value!="INSERT") {
-			if(!confirm("수정하신 데이터를 저장하시겠습니까?")) return;
-			document.frm.RetrieveFlag.value ="UPDATE";
-		} else {
-			if(!confirm("작성하신 데이터를 저장하시겠습니까?")) return;
-			document.frm.RetrieveFlag.value="INSERT"
-		}
-
-		document.frm.target = "_self";
-		document.frm.action = "orde_orde_edit_proc.php";
-		document.frm.submit();
+function Save_Click(int_type) {
+	if (int_type != 3) {
+		if (ValidChk() == false) return;
 	}
-	function ValidChk()	{
-		var f = document.frm;
 
-		if(chkSpace(f.str_sdate.value)){
-	       	alert("\n기간이 입력되지 않았습니다");
-	       	f.str_sdate.focus();
-	        return false;
-	   	}
-		if(chkSpace(f.str_edate.value)){
-	       	alert("\n기간이 입력되지 않았습니다");
-	       	f.str_edate.focus();
-	        return false;
-	   	}
-		return true;
+	if (document.frm.RetrieveFlag.value != "INSERT") {
+		if (!confirm("수정하신 데이터를 저장하시겠습니까?")) return;
+		document.frm.RetrieveFlag.value = "UPDATE";
+	} else {
+		if (!confirm("작성하신 데이터를 저장하시겠습니까?")) return;
+		document.frm.RetrieveFlag.value = "INSERT"
 	}
+
+	document.frm.target = "_self";
+	document.frm.action = "orde_orde_edit_proc.php";
+	document.frm.submit();
+}
+function ValidChk() {
+	var f = document.frm;
+
+	if (chkSpace(f.str_sdate.value)) {
+		alert("\n기간이 입력되지 않았습니다");
+		f.str_sdate.focus();
+		return false;
+	}
+	if (chkSpace(f.str_edate.value)) {
+		alert("\n기간이 입력되지 않았습니다");
+		f.str_edate.focus();
+		return false;
+	}
+	return true;
+}
