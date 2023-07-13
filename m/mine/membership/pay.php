80bde64002a874171581435c53d86d1d736eeb36
Hai Chi <chenxidev1129@gmail.com>
2023-07-13 11:25:27 +0800

    update

--- m/mine/membership/pay.php
+++ m/mine/membership/pay.php
@@ -208,10 +208,7 @@ $payment_Data = mysql_fetch_assoc($arr_Rlt_Data);
                         </div>
                         <hr class="border-t-[1px] border-white">
                         <div class="flex-1 flex justify-end items-center px-[15px]">
-                            <?php
-                            $card_number_array = array_chunk($payment_Data['STR_CARDNO'], 4);
-                            ?>
-                            <p class="font-bold text-xs leading-[10px] text-white">**** **** **** <?= $card_number_array[3] ?: '' ?></p>
+                            <p class="font-bold text-xs leading-[10px] text-white">**** **** **** <?= $payment_Data['STR_CARDNO'] ? substr($payment_Data['STR_CARDNO'], -4) : '' ?></p>
                         </div>
                     </div>
                 <?php
