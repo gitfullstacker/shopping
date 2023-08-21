15218eb1c5bbb1023ed9abc33422e000c6683199
Hai Chi <chenxidev1129@gmail.com>
2023-08-21 11:13:40 +0800

    update

--- db_convert/product_detail_images.php
+++ db_convert/product_detail_images.php
@@ -13,11 +13,10 @@ $SQL_QUERY =    'SELECT
 
 $good_list_result = mysql_query($SQL_QUERY);
 
-var_dump ($SQL_QUERY);
-exit;
-
 while ($row = mysql_fetch_assoc($good_list_result)) {
     $SQL_QUERY = 'UPDATE ' . $Tname . 'comm_goods_master SET STR_CONTENTS=REPLACE(STR_CONTENTS, "http://ablanccompany.cafe24.com", "https://ablanc.co.kr") WHERE STR_GOODCODE="' . $row['STR_GOODCODE'] . '"';
+    var_dump ($SQL_QUERY);
+exit;
     mysql_query($SQL_QUERY);
 }
 ?>
