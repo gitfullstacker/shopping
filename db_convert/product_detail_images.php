d09b9c147ede92d5287ba401f920e545bcee8d6e
Hai Chi <chenxidev1129@gmail.com>
2023-08-21 11:06:28 +0800

    update

---
+++ db_convert/product_detail_images.php
@@ -0,0 +1,20 @@
+<? include_once $_SERVER['DOCUMENT_ROOT'] . "/pub/inc/comm.php"; ?>
+
+<?php
+// Set a higher maximum execution time (e.g., 600 seconds)
+set_time_limit(60000);
+
+$SQL_QUERY =    'SELECT 
+                    A.*, B.*
+                FROM 
+                    `' . $Tname . 'comm_goods_master` A
+                WHERE 
+                    A.STR_CONTENTS="%http://ablanccompany.cafe24.com%"';
+
+$good_list_result = mysql_query($SQL_QUERY);
+
+while ($row = mysql_fetch_assoc($good_list_result)) {
+    $SQL_QUERY = 'UPDATE ' . $Tname . 'comm_goods_master SET STR_CONTENTS=REPLACE(STR_CONTENTS, "http://ablanccompany.cafe24.com", "https://ablanc.co.kr") WHERE STR_GOODCODE=' . $row['STR_GOODCODE'];
+    mysql_query($SQL_QUERY);
+}
+?>
